4055d7f411b64f5fca3dbcf1fc55ce52ee5f959d
ikpak <inkha.pak@gmail.com>
2020-05-11 21:38:44 +0700

    edit

--- main.js
+++ main.js
@@ -2,18 +2,21 @@
 let amount = prompt("Enter the amount")
 
 // 2. define From currency
-let from = prompt("Choose your currency")
+let from = prompt("Choose your currency").toUpperCase()
+
 
 // 3. define To currency
-let to = prompt("Choose the currency you want to convert your amount to")
+let to = prompt("Choose the currency you want to convert your amount to").toUpperCase()
 
-// 4. define exchange rate (23,161.39)
+// Exchange rates
 // USD to VND
 let exchangeRateUV = 23192.93
 // JPY to USD
 let exchangeRateJU = 0.0093
+// JPY to VND
+let exchangeRateJV = 216.49
 
-// 5. define result
+// Define result
 let result = 0
 
 if(from === "USD" && to === "VND") {
@@ -25,20 +28,40 @@ if(from === "USD" && to === "VND") {
 } else if(from === "JPY" && to === "USD") {
     result = jpyToUsd();
     console.log(new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', minimumFractionDigits: 2 }).format(jpyToUsd()))
+} else if(from === "USD" && to === "JPY") {
+    result = usdToJpy();
+    console.log(new Intl.NumberFormat('ja-JP', { style: 'currency', currency: 'JPY' }).format(usdToJpy()))
+} else if(from === "JPY" && to === "VND") {
+    result = jpyToVnd();
+    console.log(new Intl.NumberFormat('it-IT', { style: 'currency', currency: 'VND' }).format(jpyToVnd()))
+} else if(from === "VND" && to === "JPY") {
+    result = vndToJpy();
+    console.log(new Intl.NumberFormat('ja-JP', { style: 'currency', currency: 'JPY' }).format(vndToJpy()))
 } else {
-    console.log("Please enter existing currency")
+    alert("Please enter existing currency")
 }
 
-// 6. define converting function
+// Calculation functions
+// USD - VND
 function usdToVnd() {
-    // 6-a. amount * currency ratio = result
     return amount * exchangeRateUV
 }
-
 function vndToUsd() {
     return amount / exchangeRateUV
 }
 
+// JPY - USD
 function jpyToUsd() {
     return amount * exchangeRateJU
+}
+function usdToJpy() {
+    return amount / exchangeRateJU
+}
+
+// JPY - VND
+function jpyToVnd() {
+    return amount * exchangeRateJV
+}
+function vndToJpy() {
+    return amount / exchangeRateJV
 }
